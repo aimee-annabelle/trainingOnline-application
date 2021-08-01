63d2c8c362ce67e15d888632631535934561faf1
Annabelle <54577405+aimee-annabelle@users.noreply.github.com>
2021-08-01 15:06:06 +0200

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-const e = require("express")
+// load env variables
+const dotenv = require("dotenv");
+dotenv.config({path:"./config/config.env"});
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const {auth} = require("./middlewares/auth")
+require("./config/db");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+// use express
+
+// importing routes
+const UserRoutes = require('./routes/user.routes');
+
+
+app.get("/",(req,res)=>{
+    res.send({message:"Welcome to the application portal"});
+});
+app.use("/api/v1/user",UserRoutes);
+
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT,console.log(`Server is running on port ${PORT} ...`))
